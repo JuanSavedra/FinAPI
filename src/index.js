5628afc3288e63a8d9a363a74517253fe423b26e
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-04-06 08:58:14 -0300

    account registration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 const express = require('express');
+const { v4: uuidv4 } = require ("uuid") 
 const app = express();
+app.use(express.json()); //Middleware
+
+/* Dados da conta
+cpf - string
+name - string
+id - uuid
+statement - []
+*/
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    })
+
+    return response.status(201).send();
+});
 
 app.listen(3333);
